02699b89d3e68178388bb8851c90e6a09aa338f3
Chrisborer <chris.borer@gmail.com>
2022-06-13 12:10:21 -0500

    update dashboard route

---
+++ controllers/api/dashRoutes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const {POst, User, Comment} = require("../models");
+const {route} = require("./homeRoutes");
+const withAuth = require("../utils/auth");
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        // Find the logged in user based on the session ID
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        console.log(user)
+
+        res.render('dashboard', {
+            user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
